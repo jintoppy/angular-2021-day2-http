3494a3016438a252938d1254000967c362e15e43
Jinto Jose <jintoppy@gmail.com>
2021-05-22 10:46:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Product } from './models/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-day2-http';
+  title = 'hello';
+  products$: Observable<Product[]> = new Observable<Product[]>();
+  constructor(private appService: AppService) {
+    this.products$ = this.appService.getProducts();
+  }
 }
